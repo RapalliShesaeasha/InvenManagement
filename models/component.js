0d9b0968fd8c0052b9f9e27a94cd975f42db6fd8
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-08-07 13:53:05 +0530

    Update component.js

--- models/component.js
+++ models/component.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const ComponentSchema = new mongoose.Schema({
     name: { type: String, required: true },
     specification: { type: String, required: true },
-    quantity: { type: String, required: true }
+    quantity: { type: Number, required: true }
 });
 
 const Component = mongoose.models.Component || mongoose.model('Component', ComponentSchema);
