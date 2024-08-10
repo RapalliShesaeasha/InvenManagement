619ca32f252229b3c5b5b119df7070219569ddfe
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-08-10 11:20:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.get('/test', (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(Server started on port ${PORT});
+    console.log(`Server started on port ${PORT}`);
 });
