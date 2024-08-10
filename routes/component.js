a108b07df954bb8efb4c236ee5c124e377355639
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-08-10 11:25:01 +0530

    Update component.js

--- routes/component.js
+++ routes/component.js
@@ -1,4 +1,4 @@
-mport express from 'express';
+import express from 'express';
 import { getComponents, addComponent } from '../controllers/componentController.js';
 
 const router = express.Router();
